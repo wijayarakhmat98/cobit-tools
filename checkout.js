fadd6c790098dd81ce014fe579b91df037a12324
Wijaya Rakhmat <wijaya.rakhmat@hotmail.com>
2024-01-25 15:49:06 +0700

    Extract callback

--- checkout.js
+++ checkout.js
@@ -3,48 +3,58 @@ import {
 }
 from 'master';
 
-import chart_control from 'control';
 import chart_graph from 'graph';
 import chart_sheet from 'sheet';
 import chart_gmo from 'gmo';
+import chart_control from 'control';
 
-function checkout(history, commit, view_control, view_graph, view_sheet, view_gmo) {
+function checkout(commit, history, view_control, view_graph, view_sheet, view_gmo) {
+	const views = [view_control, view_graph, view_sheet, view_gmo];
 	chart_graph(view_graph, history);
 	chart_sheet(view_sheet, commit, () => chart_gmo(view_gmo));
 	chart_gmo(view_gmo);
-	const callback = {
-		'edit': () => checkout(history, [...commit, null], view_control, view_graph, view_sheet, view_gmo),
-		'discard': () => checkout(history, [commit[0]], view_control, view_graph, view_sheet, view_gmo),
-		'save': () => {
-			const new_commit = {
-				'id': history.length,
-				'parent': commit[0],
-				'merge': [],
-				'change': mst_df1.reduce((c, d) => {
-					const v = JSON.parse(document.querySelector(
-						`input[name="df1 ${d.id} value"]:checked`
-					).value);
-					if (v.from != null)
-						return c;
-					c.push({
-						'id': d.id,
-						'inherit': false,
-						'value': v.value,
-						'comment': document.querySelector(
-							`textarea[name="df1 ${d.id} comment"]`
-						).value
-					});
-					return c;
-				}, []),
-				'author': document.querySelector('textarea[name="username"]').value,
-				'description': '',
-				'timestamp': Date.now()
-			};
-			history.push(new_commit);
-			checkout(history, [new_commit], view_control, view_graph, view_sheet, view_gmo);
-		}
+	chart_control(view_control, view_graph, commit, {
+		'edit': () => edit(commit, history, ...views),
+		'discard': () => discard(commit, history, ...views),
+		'save': () => save(commit, history, ...views)
+	});
+}
+
+function edit(commit, ...argv) {
+	checkout([...commit, null], ...argv);
+}
+
+function discard(commit, ...argv) {
+	checkout([commit[0]], ...argv);
+}
+
+function save(commit, history, ...argv) {
+	const new_commit = {
+		'id': history.length,
+		'parent': commit[0],
+		'merge': [],
+		'change': mst_df1.reduce((c, d) => {
+			const v = JSON.parse(document.querySelector(
+				`input[name="df1 ${d.id} value"]:checked`
+			).value);
+			if (v.from != null)
+				return c;
+			c.push({
+				'id': d.id,
+				'inherit': false,
+				'value': v.value,
+				'comment': document.querySelector(
+					`textarea[name="df1 ${d.id} comment"]`
+				).value
+			});
+			return c;
+		}, []),
+		'author': document.querySelector('textarea[name="username"]').value,
+		'description': '',
+		'timestamp': Date.now()
 	};
-	chart_control(view_control, view_graph, commit, callback);
+	history.push(new_commit);
+	checkout([new_commit], history, ...argv);
 }
 
 export default checkout;
--- main.js
+++ main.js
@@ -5,7 +5,7 @@ function main(history, control_id, graph_id, sheet_id, gmo_id) {
 	let view_graph = document.getElementById(graph_id);
 	let view_sheet = document.getElementById(sheet_id);
 	let view_gmo = document.getElementById(gmo_id);
-	checkout(history, [history[0]], view_control, view_graph, view_sheet, view_gmo);
+	checkout([history[0]], history, view_control, view_graph, view_sheet, view_gmo);
 };
 
 export default main;
